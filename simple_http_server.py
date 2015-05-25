a9c0e516fe77b20aacbee38ac87ebe54064cb860
JensDiemer <git@jensdiemer.de>
2015-05-25 18:54:14 +0200

    add simple_http_server.py

---
+++ simple_http_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+try:
+    # python 3
+    from http.server import SimpleHTTPRequestHandler
+    import socketserver
+except ImportError:
+    # python 2
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    import SocketServer as socketserver
+
+
+ADDR = "127.0.0.1"
+PORT = 8000
+
+httpd = socketserver.TCPServer(
+    (ADDR, PORT),
+    SimpleHTTPRequestHandler
+)
+
+print("serving at http://%s:%s" % (ADDR, PORT))
+httpd.serve_forever()
+
