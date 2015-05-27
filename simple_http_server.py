8d5161471947ed91fe095a779eda0ff2e4357d67
JensDiemer <git@jensdiemer.de>
2015-05-27 13:23:12 +0200

    Add path info in error message

--- simple_http_server.py
+++ simple_http_server.py
@@ -13,9 +13,23 @@ except ImportError:
 ADDR = "127.0.0.1"
 PORT = 8000
 
+
+class RequestHandler(SimpleHTTPRequestHandler):
+    """
+    Add path info in error message
+    e.g.: 404 - File not found
+    """
+    def send_error(self, code, message=None):
+        path_info = repr(self.path)
+        if message is None:
+            message = path_info
+        else:
+            message += " - %s" % path_info
+        super(RequestHandler, self).send_error(code, message)
+
+
 httpd = socketserver.TCPServer(
-    (ADDR, PORT),
-    SimpleHTTPRequestHandler
+    (ADDR, PORT), RequestHandler
 )
 
 print("serving at http://%s:%s" % (ADDR, PORT))
